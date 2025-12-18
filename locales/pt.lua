5a794af42353ad9b760903182cae0e07a84cd756
Mist the Goat <62227356+Mistextreme@users.noreply.github.com>
2025-12-18 18:41:03 +0000

    Upload: locales/pt.lua

---
+++ locales/pt.lua
@@ -0,0 +1,15 @@
+Locales = {}
+
+Locales['pt'] = {
+    ['not_allowed'] = "Não tens permissão para usar este comando.",
+    ['edit_mode_on'] = "Modo de edição ~g~ATIVADO~s~.",
+    ['edit_mode_off'] = "Modo de edição ~r~DESATIVADO~s~.",
+    ['laser_info'] = "Aponte o laser para o veículo. [E] Selecionar Bone | [G] Cancelar",
+    ['bone_selected'] = "Bone selecionado: %s. Ajuste a rampa agora.",
+    ['controls_info'] = "Setas: Mover | PgUp/PgDn: Altura | ENTER: Confirmar",
+    ['config_saved'] = "Configuração impressa na consola F8!",
+    ['winch_attach'] = "Cabo preso com sucesso.",
+    ['winch_too_far'] = "O veículo está demasiado longe.",
+    ['no_vehicle_found'] = "Nenhum veículo detectado.",
+    ['ramp_deployed'] = "Rampa movimentada.",
+}
